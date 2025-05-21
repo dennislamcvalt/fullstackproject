7f522fa797ebb0ca3d209933f6b34cd6c28f10d8
Dennis (Course Submission) <51769380+dennislamcvalt@users.noreply.github.com>
2025-05-21 13:31:27 +0800

    Delete SkillSnap.Client directory

--- SkillSnap.Client/Models/Skill.cs
+++
@@ -1,9 +0,0 @@
-namespace SkillSnap.Client.Models;
-
-public class Skill
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Level { get; set; } = string.Empty;
-    public int PortfolioUserId { get; set; }
-}
